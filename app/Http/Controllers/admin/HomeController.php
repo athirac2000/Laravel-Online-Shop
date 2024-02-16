5bdb1eb820540c5a37c91038fd917eaac7f0fb35
athirac2000 <athira.plr2000@gmail.com>
2024-02-16 23:54:27 +0530

    admin dashboard setting and logout

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -9,15 +9,22 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     //
-    public function index(){
-       $admin =  Auth::guard('admin')->user(); 
 
-        echo 'Welcome'.$admin->name.' <a href="'.route('admin.logout').'">Logout</a>';
+    public function index(){
+        return view('admin.dashboard');
+    //    $admin =  Auth::guard('admin')->user();
+
+    //     echo 'Welcome'.$admin->name.' <a href="'.route('admin.logout').'">Logout</a>';
+
     }
 
     public function logout(){
-        Auth::guard('admin')->logout(); 
+        Auth::guard('admin')->logout();
         return redirect()->route('admin.login');
 
     }
+    
+
+
+
 }
